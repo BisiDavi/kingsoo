97e8d6856aad7af84e38e2c8d762a5b5edd8a38e
Olubisi David <oludavidconnect@gmail.com>
2020-07-15 13:15:21 +0100

    .

---
+++ server/schema/userSchema.js
@@ -0,0 +1,6 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = graphql;
+
+const UserType = new GraphQLObjectType({
+  
+})
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/userSchema.js');
+const connectDB = require('./models/db');
 
 const app = express();
 
+connectDB();
+
 const PORT = 5000;
 
-app.use('/graphql', graphqlHTTP{
+app.use('/graphql', graphqlHTTP({
   graphiql: true,
   schema
-});
+}));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
